2e818dfc89ad98a75ddf18a0c5e8ce2b371edb67
Rdinaft <rdinaft@mail.ru>
2023-05-22 20:09:58 +0300

    Unplayable version

--- main.py
+++ main.py
@@ -1,55 +1,90 @@
 import random
 
 
-field_coordinates = {'top_left': ' ', 'top_middle': ' ', 'top_right': ' ', 'center_left': ' ', 'center_middle': ' ', 'center_right': ' ', 'bottom_left': ' ', 'bottom_middle': ' ', 'bottom_right': ' '}
-
-def greetings():
-    print('Welcome to Tic-Tac-Toe!')
-    chosen = input('Choose your side (random, naughts, crosses): ').lower()
-    return chosen
-
-def help(chosen):
-    if chosen == 'help':
-        return print(field_coordinates)
-
-def choose_side(chosen):
-    if chosen == 'random':
-        chosen_side = random.choice(['naughts', 'crosses'])
-    else:
-        chosen_side = chosen
-    return chosen_side
-
-def player_sign(chosen_side):
-    if chosen_side == 'naughts':
-        sign = 'O'
-    else:
-        sign = 'X'
-    return sign
-
-def first_turn(chosen_side):
-    if chosen_side == 'crosses':
-        return True
-    else:
-        pass
-
-def player_turn(sign):
-    move = input('Enter coordinate: ').lower()
-    if move in field_coordinates.keys():
-        field_coordinates[move] = sign
-        return field_coordinates[move]
-
-def draw_field():
+class Game:
+    field_coordinates = {'top_left': ' ', 'top_middle': ' ', 'top_right': ' ', 'center_left': ' ', 'center_middle': ' ', 'center_right': ' ', 'bottom_left': ' ', 'bottom_middle': ' ', 'bottom_right': ' '}
+    sign_list = ['naughts', 'crosses']
+    end = False
     field = [
-        [field_coordinates['top_left'], '|', field_coordinates['top_middle'], '|', field_coordinates['top_right']],
-        ['---------------------'],
-        [field_coordinates['center_left'], '|', field_coordinates['center_middle'], '|', field_coordinates['center_right']],
-        ['---------------------'],
-        [field_coordinates['bottom_left'], '|', field_coordinates['bottom_middle'], '|', field_coordinates['bottom_right']],
-        ]
-    return print(*field, sep='\n')
+            [field_coordinates['top_left'], '|', field_coordinates['top_middle'], '|', field_coordinates['top_right']],
+            ['---------------------'],
+            [field_coordinates['center_left'], '|', field_coordinates['center_middle'], '|', field_coordinates['center_right']],
+            ['---------------------'],
+            [field_coordinates['bottom_left'], '|', field_coordinates['bottom_middle'], '|', field_coordinates['bottom_right']],
+            ]
+
+    def greetings(self) -> str:
+        print('Welcome to Tic-Tac-Toe!')
+        chosen = input('Choose your side (random, naughts, crosses): ').lower()
+        return chosen
+
+    def choose_side(self, chosen: str) -> str:
+        if chosen == 'random':
+            chosen_side = random.choice(self.sign_list)
+        else:
+            chosen_side = chosen
+        self.sign_list.remove(chosen_side)
+        print(f'Okay, you choose {chosen_side}!')
+        return chosen_side
+
+    def signs_definition(self, chosen_side: str) -> tuple[str, str]:
+        if chosen_side == self.sign_list[0]:
+            player_sign = 'O'
+            computer_sign = 'X'
+        else:
+            player_sign = 'X'
+            computer_sign = 'O'
+        return player_sign, computer_sign
+
+    def first_turn(self, chosen_side: str) -> bool | None:
+        if chosen_side == 'crosses':
+            print('Your turn!')
+            return True
+        else:
+            return print('Computer turn!')
+
+    def player_turn(self, player_sign: str) -> None:
+        move = input('Enter coordinate: ').lower()
+        if move in self.field_coordinates.keys() and self.field_coordinates[move] == ' ':
+            self.field_coordinates[move] = player_sign
+
+    def computer_turn(self, computer_sign: str) -> None:
+        turn_not_over = True
+        while turn_not_over:
+            coordinate, placed_sign = random.choice(list(self.field_coordinates.items()))
+            print(coordinate, placed_sign)
+            if placed_sign == ' ' in self.field_coordinates:
+                self.field_coordinates[coordinate] = computer_sign
+            turn_not_over = False
+
+    def end_game(self) -> None:
+        if ' ' not in self.field_coordinates.values():
+            self.end = True
+
+    def draw_field(self) -> None:
+        return print(*self.field, sep='\n')
+
+
+class Play():
+    def play_game(self) -> None:
+        chosen = Game().greetings()
+        chosen_side = Game().choose_side(chosen)
+        player_sign, computer_sign = Game().signs_definition(chosen_side)
+        first_turn = Game().first_turn(chosen_side)
+
+        while Game().end == False:
+            Game().end_game()
+            if first_turn:
+                Game().draw_field()
+                Game().player_turn(player_sign)
+                Game().draw_field()
+                Game().computer_turn(computer_sign)
+            else:
+                Game().draw_field()
+                Game().computer_turn(computer_sign)
+                Game().draw_field()
+                Game().player_turn(player_sign)
 
 
 if __name__ == '__main__':
-    #draw_field()
-    print(player_turn('X'))
-    pass
+    Play().play_game()
